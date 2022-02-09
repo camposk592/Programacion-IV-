e3f4104238db03df882a7993c9e35b5caaec4a1a
Kevin Campos <camposk697@gmail.com>
2022-02-09 14:21:06 -0600

    Actualizado

--- main.js
+++ main.js
@@ -5,8 +5,10 @@ if(!db_sistema){
 var app = new Vue({
     el: '#appCliente',
     data: {
+        clientes: [],
+        buscar: '',
         cliente: {
-            accion: '',
+            accion: 'nuevo',
             msg : '',
             idCliente: '',
             codigo: '',
@@ -17,17 +19,59 @@ var app = new Vue({
         },
     },
     methods: {
+        buscarCliente(){
+            /*if( this.buscar.trim().length>0 ){
+                this.clientes = this.clientes.filter(item=>item.nombre.toLowerCase().indexOf(this.buscar.toLowerCase())>=0);
+            } else {
+                this.obtenerClientes();
+            }*/
+            this.obtenerClientes(this.buscar);
+        },
         guardarCliente(){
+            let sql = '',
+                parametros = [];
+            if(this.cliente.accion == 'nuevo'){
+                sql = 'INSERT INTO clientes (codigo, nombre, direccion, telefono, dui) VALUES (?,?,?,?,?)';
+                parametros = [this.cliente.codigo,this.cliente.nombre,this.cliente.direccion,this.cliente.telefono,this.cliente.dui];
+            }else if(this.cliente.accion == 'modificar'){
+                sql = 'UPDATE clientes SET codigo=?, nombre=?, direccion=?, telefono=?, dui=? WHERE idCliente=?';
+                parametros = [this.cliente.codigo,this.cliente.nombre,this.cliente.direccion,this.cliente.telefono,this.cliente.dui,this.cliente.idCliente];
+            }else if(this.cliente.accion == 'eliminar'){
+                sql = 'DELETE FROM clientes WHERE idCliente=?';
+                parametros = [this.cliente.idCliente];
+            }
             db_sistema.transaction(tx=>{
-                tx.executeSql('INSERT INTO clientes (codigo, nombre, direccion, telefono, dui) VALUES (?,?,?,?,?)',
-                [this.cliente.codigo, this.cliente.nombre, this.cliente.direccion, this.cliente.telefono, 
-                    this.cliente.dui],
+                tx.executeSql(sql,
+                    parametros,
                 (tx, results)=>{
-                    this.cliente.msg = 'Cliente guardado con exito';
+                    this.cliente.msg = 'Cliente procesado con exito';
                     this.nuevoCliente();
+                    this.obtenerClientes();
                 },
                 (tx, error)=>{
                     this.cliente.msg = `Error al guardar el cliente ${error.message}`;
+                });
+            });
+        },
+        modificarCliente(cliente){
+            this.cliente = cliente;
+            this.cliente.accion = 'modificar';
+        },
+        eliminarCliente(cliente){
+            if( confirm(`¿Esta seguro de eliminar el cliente ${cliente.nombre}?`) ){
+                this.cliente.idCliente = cliente.idCliente;
+                this.cliente.accion = 'eliminar';
+                this.guardarCliente();
+            }
+        },
+        obtenerClientes(busqueda=''){
+            db_sistema.transaction(tx=>{
+                tx.executeSql(`SELECT * FROM clientes WHERE nombre like "%${busqueda}%" OR codigo like "%${busqueda}%"`, [], (tx, results)=>{
+                    this.clientes = results.rows;
+                    /*this.clientes = [];
+                    for(let i=0; i<results.rows.length; i++){
+                        this.clientes.push(results.rows.item(i));
+                    }*/
                 });
             });
         },
@@ -48,5 +92,6 @@ var app = new Vue({
         }, err=>{
             console.log('Error al crear la tabla de clientes', err);
         });
+        this.obtenerClientes();
     }
 });
