370e0feba881a1c6656ce168fe978fe1b41a78ee
Kevin Campos <camposk697@gmail.com>
2022-04-01 21:27:51 -0600

    Error corregido gracias a Dios

--- sistema_academico_a1/resources/views/welcome.blade.php
+++ sistema_academico_a1/resources/views/welcome.blade.php
@@ -8,17 +8,18 @@
     <title>Laravel</title>
 
     <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
+    <!-- Styles -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">:::SISTEMA ACADEMICO:::</a>
+                <a class="navbar-brand" href="#">::.. SISTENA ACADEMICO -- UGB ..::</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -26,34 +27,33 @@
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link active" href="#">Inicio</a>
+                            <a class="nav-link" href="#">Inicio</a>
                         </li>
                         <li class="nav-item">
                             <a @click="abrirForm('alumno')" class="nav-link" href="#">Alumnos</a>
                         </li>
                         <li class="nav-item">
-                            <a @click="abrirForm('Docente')" class="nav-link" href="#">Docentes</a>
+                            <a @click="abrirForm('docente')" class="nav-link" href="#">Docentes</a>
                         </li>
                         <li class="nav-item">
-                            <a @click="abrirForm('Materia')" class="nav-link" href="#">Materia</a>
+                            <a @click="abrirForm('materia')" class="nav-link" href="#">Materias</a>
                         </li>
                         <li class="nav-item">
-                            <a @click="abrirForm('Matricula')" class="nav-link" href="#">Matricula</a>
+                            <a @click="abrirForm('matricula')" class="nav-link" href="#">Matricula</a>
                         </li>
                         <li class="nav-item">
-                            <a @click="abrirForm('Inscripcion')" class="nav-link" href="#">Inscripcion</a>
+                            <a @click="abrirForm('inscripcion')" class="nav-link" href="#">Inscripcion</a>
                         </li>
                         <li class="nav-item">
-                            <a @click="abrirForm('notas')" class="nav-link" href="#">Notas</a>
+                            <a @click="abrirForm('nota')" class="nav-link" href="#">Notas</a>
                         </li>
-
                     </ul>
                 </div>
             </div>
         </nav>
-        <alumno-component v-bind:form="forms" ref="alumno" v-show="forms['alumno'].mostrar"></alumno-component>
+        <alumno-component v-bind:form="forms" ref="alumno" v-show="forms['alumno'].mostrar" ></alumno-component>
     </div>
-    <script scr="{{asset('js/app.js')}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 
 </html>
