1d54e61013982d5f9616d77e5ea2821e82aa4b23
Kevin Campos <camposk697@gmail.com>
2022-05-20 14:45:17 -0600

    Practica con MongoDB

--- sistema_academico_a1/server/index.js
+++ sistema_academico_a1/server/index.js
@@ -1,6 +1,9 @@
 const express = require('express'),
     server = express(),
+    mongodb = require('mongodb').MongoClient,
+    url ='mongodb://localhost:27017',
     port = 3001;
+    
 server.use(express.json());
 
 server.get('/chat', function(req, resp){
@@ -8,10 +11,33 @@ server.get('/chat', function(req, resp){
 });
 
 server.get('/usuarios', function(req, resp){
-    console.log(req.body);
-    resp.send('Bienvenido al sitio de chats');
+    mongodb.connect(url,function(err,client){
+        if(err) resp.send(err);
+        const db = client.db('tartas');
+        db.collection('personal').find().toArray(function(err,data){
+            if(err) resp.send(err);
+            resp.send(data);
+        });
+    });
+    
+});
+
+server.get('/usuarios/nuevo',function(req,resp){
+    resp.sendFile(__dirname + '/index.html');
+});
+
+server.post('/usuarios/save', function(req,resp){
+    mongodb.connect(url,function(err,client){
+       if(err) resp.send(err)
+       const db = client.db('tartas');
+       db.collection('personal').insertOne(req.body,function(err,result){
+           if(err) resp.send(err);
+           resp.send(result);
+
+       });
+    });
 });
 
 server.listen(port, function(event){
-    console.log(`Server running on port ${port}`);
+    console.log('Server running on port 3001');
 });
