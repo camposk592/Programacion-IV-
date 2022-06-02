1ee356f16ace81b0ddfbbba9a6b951a600d930b7
Kevin Campos <78324289+camposk592@users.noreply.github.com>
2022-06-02 14:36:10 -0600

    Add files via upload
    subir

--- server/index.js
+++ server/index.js
@@ -1,43 +1,79 @@
 const express = require('express'),
     server = express(),
     mongodb = require('mongodb').MongoClient,
-    url ='mongodb://localhost:27017',
-    port = 3001;
-    
+    url = 'mongodb://localhost:27017',
+    port = 3001,
+    http = require('http').Server(server)
+    io = require('socket.io')(http,{
+        allowEIO3: true,
+        cors:{
+            origin:['http://localhost:8000'],
+            credential: true
+        }
+
+    });
+
+
+
+
 server.use(express.json());
+
+io.on('connection',function(socket){
+    console.log('Conectado via socket');
+
+    socket.on('historial', function(e){
+        mongodb.connect(url,function(err,client){
+            if(err) console.log(err);
+            db.collection('chat').find().toArray(function(err,chats){
+                if(err) console.log(err);
+                socket.emit('historial',chats);
+            });
+        });
+    });
+    socket.on('chat',function(chat){
+        mongodb.connect(url, function(err, client){
+            if(err) console.log(err);
+            const db=client.db('chatDB');
+            db.collection('chat').insertOne(chat).then(result=>{
+                io.emit('chat',chat);
+            }).catch(err=>{
+                console.log(err);
+            });
+        });
+    });
+});
 
 server.get('/chat', function(req, resp){
     resp.send('Hola Mundo');
 });
 
-server.get('/usuarios', function(req, resp){
-    mongodb.connect(url,function(err,client){
+server.get('/usuarios/list', function(req, resp){
+    mongodb.connect(url, function(err, client){
         if(err) resp.send(err);
         const db = client.db('prueba');
-        db.collection('usuarios').find().toArray(function(err,data){
+        db.collection('usuarios').find({}).toArray(function(err,data){
             if(err) resp.send(err);
             resp.send(data);
         });
     });
-    
 });
-
-server.get('/usuarios/nuevo',function(req,resp){
+server.get('/usuarios/nuevo', function(req, resp){
     resp.sendFile(__dirname + '/index.html');
 });
-
-server.post('/usuarios/save', function(req,resp){
-    mongodb.connect(url,function(err,client){
-       if(err) resp.send(err)
-       const db = client.db('prueba');
-       db.collection('usuarios').insertOne(req.body,function(err,result){
-           if(err) resp.send(err);
-           resp.send(result);
-
-       });
+server.post('/usuarios/save', function(req, resp){
+    mongodb.connect(url, function(err, client){
+        if(err) resp.send(err);
+        const db = client.db('prueba');
+        db.collection('usuarios').insertOne(req.body,function(err, result){
+            if(err) resp.send(err);
+            resp.send(result);
+        });
     });
 });
 
-server.listen(port, function(event){
+/*server.listen(port, function(event){
     console.log('Server running on port 3001');
+});*/
+http.listen(port, function(e){
+    console.log(`Server corriendo en puerto ${port}`);
 });
